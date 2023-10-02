0581aa2eca88b87ff5ca91f76fa22eb2ab0c87cd
lakshayarora8 <109210694+lakshayarora8@users.noreply.github.com>
2023-10-02 22:14:35 +0530

    Time: 35 ms (47.75%), Space: 13.9 MB (7.80%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -11,15 +11,8 @@ public:
             else if (colors[i] == 'B' && colors[i-1]== 'B' && colors[i+1]== 'B')
                 bob++;
         }
-        if( alice == 0) return false;
-        while(alice!=0 && bob!=0)
-        {
-            alice--;
-            bob--;
-            if(alice <= 0 ) return false;
-        }
         
-        return true;
+        return alice>bob;
         
     }
 };
