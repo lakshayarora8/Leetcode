3878426b7879c7927ccf6c46cd5deef6aaea4671
lakshayarora8 <109210694+lakshayarora8@users.noreply.github.com>
2023-11-04 14:06:58 +0530

    Added solution - LeetHub

--- Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.cpp
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.cpp
@@ -9,18 +9,19 @@ class Solution{
     int lenOfLongSubarr(int A[],  int N, int K) 
     { 
         unordered_map<int,int> mp;
-        int ans=0,sum=0;
+        
+        int sum=0,ans=0;
         mp[0]=1;
         for(int i=0;i<N;i++)
         {
             sum+=A[i];
             
-            if( mp[sum-K] )
-            ans=max(ans, i+2- mp[sum-K]);
+            if(mp [sum -K])
+            ans=max(ans, i+2 - mp[sum-K]);
+            
             if(!mp[sum])
             mp[sum]=i+2;
         }
-        
         return ans;
     } 
 
