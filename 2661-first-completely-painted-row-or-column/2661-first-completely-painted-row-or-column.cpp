838576df845dc55f485196723bb04f56211e8978
lakshayarora8 <109210694+lakshayarora8@users.noreply.github.com>
2023-11-11 10:35:44 +0530

    Time: 448 ms (17.55%), Space: 158.9 MB (61.95%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        map<int,int> mp;
         
-        unordered_map<int, pair<int,int >> mp;
-        unordered_map<int,int> row,col;
-        int rows= mat.size();
-        int cols =mat[0].size();
-        for(int i=0; i<rows; i++)
-        {
-            for(int j=0; j<cols; j++)
-            {
-                mp[ mat[i][j] ]= {i,j};
+        int n=mat.size(), m=mat[0].size(), ans=1e9;
+        for (int i=0;i<arr.size();i++)mp[arr[i]]=i;
+        for (int i=0;i<n;i++){
+            int c=0;
+            for (int j=0;j<m;j++){
+                c=max(c,mp[mat[i][j]]);
             }
+            ans=min(ans,c);
         }
-        
-        for( int i=0;i< arr.size(); i++)
-        {
-            int value= arr[i];
-            int r= mp[ value ].first;
-            int c= mp[ value ].second;
-            
-            row[r]++;
-            col[c]++;
-            
-            if(row[r]==cols || col[c]== rows)
-                return i;
+        for (int i=0;i<m;i++){
+            int c=0;
+            for (int j=0;j<n;j++){
+                c=max(c,mp[mat[j][i]]);
+            }
+            ans=min(ans,c);
         }
-        
-        return -1;
-        
+        return ans;
     }
 };
